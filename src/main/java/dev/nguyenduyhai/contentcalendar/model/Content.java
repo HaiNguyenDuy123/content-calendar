f6d07d5a18ff05e9c63a297f8e625704fbc3647f
Hải Nguyễn Duy <nguyenduyhai0018@gmail.com>
2023-10-05 19:38:20 +0700

    update commit

--- src/main/java/dev/nguyenduyhai/contentcalendar/model/Content.java
+++ src/main/java/dev/nguyenduyhai/contentcalendar/model/Content.java
@@ -3,14 +3,15 @@ package dev.nguyenduyhai.contentcalendar.model;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
 
 public record Content(
         @Id
         Integer id,
-        @NotBlank
         String title,
+        @Column(value = "description")
         String desc,
         Status status,
         Type contentType,
