670c30f07ad485ee0e576a83e58d210d8873dea4
Hải Nguyễn Duy <nguyenduyhai0018@gmail.com>
2023-10-11 16:15:04 +0700

    update commit

--- src/main/java/dev/nguyenduyhai/contentcalendar/config/ContentCalendarProperties.java
+++ src/main/java/dev/nguyenduyhai/contentcalendar/config/ContentCalendarProperties.java
@@ -3,5 +3,5 @@ package dev.nguyenduyhai.contentcalendar.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(value = "cc")
-public record ContentCalendarProperties(String hai, String welcomemessage) {
+public record ContentCalendarProperties(String nguyenduyhai, String welcome_message, String function) {
 }
